102186c2995f27cbdcdb094bb4259dce5b39049f
syed zeeshan <tokt282@gmail.com>
2025-09-04 12:43:10 +0000

    Let's add subtle grid animation to the background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,22 @@ import { Leaderboards } from '@/components/landing/leaderboards';
 import { Logos } from '@/components/landing/logos';
 import { TopTalent } from '@/components/landing/top-talent';
 import { AnimatedSection } from '@/components/shared/animated-section';
-import { SocialIconsAnimation } from '@/components/landing/social-icons-animation';
+import { GridPattern } from '@/components/ui/grid-pattern';
+import { cn } from '@/lib/utils';
 
 export default function Home() {
   return (
     <div className="relative flex flex-col gap-16 md:gap-24 overflow-hidden">
-      <SocialIconsAnimation />
+       <GridPattern
+        width={30}
+        height={30}
+        x={-1}
+        y={-1}
+        className={cn(
+          '[mask-image:linear-gradient(to_bottom,white_20%,transparent_90%)]',
+          'absolute inset-0 z-0'
+        )}
+        />
       <div className="relative z-10">
         <HeroSection />
         <div className="container mx-auto px-4">
