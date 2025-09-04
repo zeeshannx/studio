55c8eea8948b0b169113456ca26f101f3dd3157f
syed zeeshan <tokt282@gmail.com>
2025-09-04 11:19:12 +0000

    Try fixing this error: `Runtime Error: Error: Attempted to call navigati

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Sheet, SheetContent, SheetTrigger, SheetHeader, SheetTitle } from '@/components/ui/sheet';
