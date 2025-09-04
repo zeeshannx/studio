3c1939945829a855ad82cd689cac964e6e96606d
syed zeeshan <tokt282@gmail.com>
2025-09-04 12:27:15 +0000

    Don't add any shadow on the edges (_for element div_)

--- src/components/landing/logos.tsx
+++ src/components/landing/logos.tsx
@@ -82,8 +82,6 @@ const Logos = ({
               ))}
             </CarouselContent>
           </Carousel>
-          <div className="pointer-events-none absolute inset-y-0 left-0 w-12 bg-gradient-to-r from-background to-transparent"></div>
-          <div className="pointer-events-none absolute inset-y-0 right-0 w-12 bg-gradient-to-l from-background to-transparent"></div>
         </div>
       </div>
     </section>
