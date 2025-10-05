2714cd23f329d349a38c2a5e13b2b280d25d2458
syed zeeshan <tokt282@gmail.com>
2025-10-05 05:25:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "credableo1-00346434-5adc9",
+  "appId": "1:597681802353:web:4365a248b9db488ddfdfd0",
+  "apiKey": "AIzaSyDnipW2JlVqCOKXK5zOxtcbFHTJQU5ey1A",
+  "authDomain": "credableo1-00346434-5adc9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "597681802353"
+};
